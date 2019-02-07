fc54f4107a3dc00a3b2b3df673ac4564fe249b44
louiejtaylor <louiejtaylor@gmail.com>
2019-02-07 11:34:10 -0500

    keep "length" around for a bit longer

--- find_contigs.R
+++ find_contigs.R
@@ -31,17 +31,18 @@ if(is.na(output))
 # annotation <- "/media/THING2/louis/09_SearchingExternalDatasets/Datasets/PositiveSamples/sunbeam_output/annotation/summary/testsample.tsv"
 # output <- "/media/THING2/louis/09_SearchingExternalDatasets/Datasets/PositiveSamples/sunbeam_output/annotation/summary/test_positive_contig_list.txt"
 
-cols_select <- c("sample", "contig", dbname)
+cols_select <- c("sample", "contig", "length", dbname)
 
 annotation_df <- read.delim(annotation) %>%
   dplyr::select(one_of(cols_select))
 
-colnames(annotation_df) <- c("sample", "contig", "hit")
+colnames(annotation_df) <- c("sample", "contig", "length", "hit")
 
 annotation_df %>%
   drop_na(hit) %>%
   group_by(sample, hit) %$%
   .[order(.$length, decreasing = TRUE),] %>%
+  dplyr::select(c(sample, contig, hit)) %>%
   write_delim(., path = output, 
                  delim = " ")
 
